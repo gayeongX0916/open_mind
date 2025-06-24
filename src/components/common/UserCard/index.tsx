c5134497783f216f88b3e5585de97e81c7c50fc0
gayeongX0916 <gayeongx0916@gmail.com>
2025-06-24 15:38:57 +0900

    [feat/#10] UserCard 컴포넌트 생성

---
+++ src/components/common/UserCard/index.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import messageGrayIcon from "@/assets/message_gray_icon.svg";
+import styles from "./index.module.scss";
+
+type UserCardProps = {
+  img: string;
+  nickname: string;
+  question: number;
+};
+
+export function UserCard({ img, nickname, question }: UserCardProps) {
+  return (
+    <div className={styles["user-card"]}>
+      <div className={styles["user-card__profile"]}>
+        <Image alt="사용자 이미지" src={img} width={60} height={60} className={styles["user-card__avatar"]} />
+        <span className={styles["user-card__nickname"]}>{nickname}</span>
+      </div>
+      <div className={styles["user-card__bottom-wrapper"]}>
+        <div className={styles["user-card__question-wrapper"]}>
+          <Image alt="받은 질문" src={messageGrayIcon} width={18} height={18} />
+          <span className={styles["user-card__question"]}>받은 질문</span>
+        </div>
+        <span className={styles["user-card__question-count"]}>
+          {question}개
+        </span>
+      </div>
+    </div>
+  );
+}
