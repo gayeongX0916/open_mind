0edb103b13ee02645b6c23f0760f39c3294be9f3
gayeongX0916 <gayeongx0916@gmail.com>
2025-06-26 16:30:06 +0900

    [feat/#24] 메인페이지 퍼블리싱

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,43 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.scss";
 import logo from "@/assets/logo.svg";
+import mainCharacter from "@/assets/main_background.svg";
+import { ArrowButton } from "@/components/common/Button";
+import { useRouter } from "next/navigation";
+import { InputField } from "@/components/common/Input";
+import { useState } from "react";
 
-export default function Home() {
-  return <main className={styles.main}></main>;
-}
+const Home = () => {
+  const router = useRouter();
+  const [value, setValue] = useState("");
+
+  return (
+    <main className={styles.main}>
+      <div className={styles["button-wrapper"]}>
+        <ArrowButton
+          mode="answer"
+          showArrow={true}
+          onClick={() => router.push("/list")}
+        >
+          질문하러 가기
+        </ArrowButton>
+      </div>
+      <div className={styles.logo}>
+        <Image src={logo} alt="로고" width={456} height={180} />
+      </div>
+      <form className={styles["input-form"]}>
+        <InputField value={value} onChange={(e) => setValue(e.target.value)} />
+        <ArrowButton mode="question" showArrow={false} onClick={() => {}}>
+          질문 받기
+        </ArrowButton>
+      </form>
+      <div className={styles["main-character"]}>
+        <Image src={mainCharacter} alt="메인 캐릭터" />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
