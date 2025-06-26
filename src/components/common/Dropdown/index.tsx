e4ea5aa81a3ca7be1d427b9c2f10b9acaab84c9c
gayeongX0916 <gayeongx0916@gmail.com>
2025-06-26 10:37:26 +0900

    [feat/#25] Dropdown 컴포넌트 수정

--- src/components/common/Dropdown/index.tsx
+++ src/components/common/Dropdown/index.tsx
@@ -4,13 +4,9 @@ import Image from "next/image";
 import styles from "./index.module.scss";
 import arrowDownGray from "@/assets/arrow_down_gray.svg";
 import arrowUp from "@/assets/arrow_up.svg";
-import editIcon from "@/assets/edit_icon.svg";
-import closeIcon from "@/assets/close_icon.svg";
-import editBlueIcon from "@/assets/edit_blue_icon.svg";
-import closeBlueIcon from "@/assets/close_blue_icon.svg";
 import { useEffect, useRef, useState } from "react";
 
-export function SortDropdown() {
+export function Dropdown() {
   const [isOpen, setIsOpen] = useState(false);
   const [selected, setSelected] = useState("이름순");
   const sortDropdownRef = useRef<HTMLDivElement>(null);
@@ -30,10 +26,10 @@ export function SortDropdown() {
   }, []);
 
   return (
-    <div ref={sortDropdownRef} className={styles["sort-dropdown__wrapper"]}>
+    <div ref={sortDropdownRef} className={styles.dropdown}>
       <button
-        className={`${styles["sort-dropdown"]} ${
-          isOpen ? styles["sort-dropdown__isOpen"] : ""
+        className={`${styles["dropdown__wrapper"]} ${
+          isOpen ? styles["dropdown__isOpen"] : ""
         }`}
         onClick={() => setIsOpen(!isOpen)}
       >
@@ -46,11 +42,11 @@ export function SortDropdown() {
         />
       </button>
       {isOpen && (
-        <div className={styles["sort-dropdown__container"]}>
+        <div className={styles["dropdown__container"]}>
           {["이름순", "최신순"].map((option) => (
             <button
               key={option}
-              className={styles["sort-dropdown__menu-button"]}
+              className={styles["dropdown__menu-button"]}
               onClick={() => {
                 setSelected(option);
                 setIsOpen(false);
@@ -61,40 +57,6 @@ export function SortDropdown() {
           ))}
         </div>
       )}
-    </div>
-  );
-}
-
-export function ActionDropdown() {
-  const actionDropdownList = [
-    { img: editIcon, label: "수정하기", clickedImg: editBlueIcon },
-    { img: closeIcon, label: "삭제하기", clickedImg: closeBlueIcon },
-  ];
-  const [selected, setSelected] = useState("");
-
-  return (
-    <div className={styles["action-dropdown"]}>
-      {actionDropdownList.map((option) => {
-        const isSelected = selected === option.label;
-        return (
-          <button
-            key={option.label}
-            className={`${styles["action-dropdown__menu-button"]} ${
-              isSelected ? styles["selected"] : ""
-            }`}
-            onClick={() => setSelected(option.label)}
-          >
-            <Image
-              src={isSelected ? option.clickedImg : option.img}
-              alt={option.label}
-              width={14}
-              height={14}
-              className={styles["default-img"]}
-            />
-            {option.label}
-          </button>
-        );
-      })}
     </div>
   );
 }
