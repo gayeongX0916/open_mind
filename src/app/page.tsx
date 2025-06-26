4ca5125a139e44b9b40ca4a7df8f49b70d3c7a92
gayeongX0916 <gayeongx0916@gmail.com>
2025-06-26 11:32:33 +0900

    [feat/#25] kebabmenu 컴포넌트 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
+import logo from "@/assets/logo.svg";
+import { KebabMenu } from "@/components/common/KebabMenu";
 
 export default function Home() {
-  return <></>;
+  return (
+    <main className={styles.main}>
+    </main>
+  );
 }
