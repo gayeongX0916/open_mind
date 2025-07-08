adc38059ae84afeac2a0afffced0ba9c8a66bb8b
gayeongX0916 <gayeongx0916@gmail.com>
2025-07-08 16:02:29 +0900

    [feat/#68] id / answer 페이지 스켈레톤 적용

--- src/app/post/[id]/answer/page.tsx
+++ src/app/post/[id]/answer/page.tsx
@@ -9,18 +9,22 @@ import { SubjectsQuestions } from "@/types/Subjects";
 import getSubjectsQuestions from "@/services/subjects/getSubjectsQuestions";
 import QuestionList from "@/components/QuestionList";
 import DeleteModal from "@/components/Modal/DeleteModal";
+import FeedCardSkeleton from "@/components/Skeleton/FeedCardSkeleton";
 
 export default function postAnswerPage() {
   const { id } = useParams();
   const [questionCount, setQuestionCount] = useState(0);
   const [questionList, setQuestionList] = useState<SubjectsQuestions[]>([]);
   const [showModal, setShowModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchQuestionList = async (id: number) => {
+      setIsLoading(true);
       const data = await getSubjectsQuestions(id);
       setQuestionList(data.results);
       setQuestionCount(data.count);
+      setIsLoading(false);
     };
 
     fetchQuestionList(Number(id));
@@ -32,21 +36,27 @@ export default function postAnswerPage() {
 
   return (
     <div className={styles["post-answer-page"]}>
-      {showModal && (
-        <DeleteModal subjectId={String(id)} onModalChange={setShowModal} />
+      {isLoading ? (
+        <FeedCardSkeleton />
+      ) : (
+        <>
+          {showModal && (
+            <DeleteModal subjectId={String(id)} onModalChange={setShowModal} />
+          )}
+          <Header subjectId={Number(id)} />
+          <div className={styles.question}>
+            <div className={styles["delete-floating-button"]}>
+              <FloatingButton mode="delete" onClick={handleOpenModal}>
+                삭제하기
+              </FloatingButton>
+            </div>
+            <QuestionList
+              questionCount={questionCount}
+              questionList={questionList}
+            />
+          </div>
+        </>
       )}
-      <Header subjectId={Number(id)} />
-      <div className={styles.question}>
-        <div className={styles["delete-floating-button"]}>
-          <FloatingButton mode="delete" onClick={handleOpenModal}>
-            삭제하기
-          </FloatingButton>
-        </div>
-        <QuestionList
-          questionCount={questionCount}
-          questionList={questionList}
-        />
-      </div>
     </div>
   );
 }
